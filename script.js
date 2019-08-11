3949afe968f9859a766661720a52dbfde43dbcb5
danil <monstr14ekss@gmail.com>
2019-08-11 13:06:02 +0300

    Training with while and do-while

--- script.js
+++ script.js
@@ -36,3 +36,41 @@ if (appData.moneyPerDay < 100) {
 } else {
     console.log('Произошла Ошибка');
 }
+
+// WHILE
+
+// let i = 0;
+
+// while (i < 0) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//     && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         i--;
+//     }
+
+//     i++;
+// }
+
+// DO-WHILE
+
+// let i = 0;
+
+// do {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//     && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         i--;
+//     }
+
+//     i++;
+// } while (i < 0);
